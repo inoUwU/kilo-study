f5ebf0030e490b001b361df396ac0ac054b0e9d7
ino <87593133+inoUwU@users.noreply.github.com>
2024-09-13 16:04:14 +0000

    🚧 Turn off echoing

--- c_app/kilo/kilo.c
+++ c_app/kilo/kilo.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
- 
-int main(){
-    printf("HelloWorld \n");
+#include <termios.h>
+#include <unistd.h>
+
+void enableRawMode()
+{
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main()
+{
+    enableRawMode();
+    char c;
+    // ファイルを1byteずつよむ
+    while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q')
+        ;
+    return 0;
 }
