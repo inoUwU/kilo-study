ba9cead47d211f541b29e0c601523cfabb53b232
ino <87593133+inoUwU@users.noreply.github.com>
2024-09-14 06:08:32 +0000

    🚧 Turn off canonical mode

--- c_app/kilo/kilo.c
+++ c_app/kilo/kilo.c
@@ -1,10 +1,24 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    // 現在のターミナルの状態を取得
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    // プログラム終了時に呼ばれる関数を登録(以前のターミナル状態を戻す)
+    atexit(disableRawMode);
+
+    // Rawモードに設定
+    struct termios raw = orig_termios;
+    // ビットを反転させてAND演算で代入する
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
